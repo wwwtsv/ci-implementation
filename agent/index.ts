d532d33b6506d1b3c0d3b4459b4ba3b5e9e2aa62
icwet <icwet@yandex.ru>
2021-05-23 22:35:57 +0300

    add yargs

--- agent/index.ts
+++ agent/index.ts
@@ -1,16 +1,22 @@
 import http from "http";
 import express from "express";
 import Logger from "./loaders/logger";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-(async function startServer() {
+(async function startAgent() {
   const app = express();
 
   const server = http.createServer(app);
 
+  const argv = yargs(hideBin(process.argv)).argv;
+
+  const port = argv.port || 3001;
+
   server
-    .listen(3010, () => {
+    .listen(port, () => {
       Logger.info(`
-      🤖 Server listening on port: 3010
+      🤖 Agent listening on port: 3010
     `);
     })
     .on("error", (error) => {
