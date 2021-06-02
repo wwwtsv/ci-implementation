d3565a8ec35275de00ac67a5e78fb9d60d8a88db
icwet <icwet@yandex.ru>
2021-06-03 00:00:25 +0300

    complete agents connections view

--- server/loaders/socket.ts
+++ server/loaders/socket.ts
@@ -5,10 +5,15 @@ import Logger from "./logger";
 export default (server: HttpServer): Server => {
   const IoServer = new Server(server);
 
-  IoServer.on("connection", () => {
+  IoServer.on("connection", (socket) => {
     const connectionsCount = IoServer.engine.clientsCount;
     Logger.info(`Connection count: ${connectionsCount}`);
-    IoServer.emit("connections", connectionsCount);
+    IoServer.emit("client:connect", connectionsCount);
+
+    socket.on("disconnect", (reason) => {
+      IoServer.emit("client:disconnect");
+      Logger.info(`Client disconnected reason: ${reason}`);
+    });
   });
 
   return IoServer;
